73b2b2b836469366eda211fb44a348ad880bbed3
francescomascellino <franesco.mascellino@gmail.com>
2023-09-22 14:21:42 +0200

    iniitial commit
    placeholder component + favicon

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <AppHeader></AppHeader>
+    <div>
+        <h1 class="text-danger text-center">APP</h1>
+    </div>
+</template>
+
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+
+    components: {
+        AppHeader
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    background-color: azure;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <h1 class="text-danger text-center">HEADER</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppHeader"
+
+}
+</script>
+
+<style lang="scss" scoped>
+div.header {
+    background-color: aqua;
+}
+</style>
